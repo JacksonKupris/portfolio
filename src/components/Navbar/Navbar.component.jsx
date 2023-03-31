0c3b4ce9adf2d6d098eb922417fbb65fb3a50b0f
JacksonKupris <jacksonkupris@gmail.com>
2023-03-30 19:20:39 -0500

    CHORE: More formatting

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,13 +1,6 @@
 import "./Navbar.style.scss";
 import { NavLink } from "react-router-dom";
 
-// const toggleNav = () => {
-//   const toggleButton = document.getElementsByClassName("toggle-button")[0];
-//   const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-//   toggleButton.addEventListener("click", () => {
-//     navbarLinks.classList.toggle("active");
-//   });
-// };
 
 const Navbar = () => {
   return (
